e9a0712ee55b5b14683a054a716be8422feadc5c
Phong Nguyen <phongnt.ptnk@gmail.com>
2020-12-18 17:50:42 +0800

    Phong keyboard

---
+++ keyboards/dz60/keymaps/phong/config.h
@@ -0,0 +1,3 @@
+#define GRAVE_ESC_GUI_OVERRIDE    # Always send Escape if GUI is pressed
+
+#define TAPPING_TERM 200 /*Reduce this from default value 200ms so that holding CAPS register as ctrl faster*/
---
+++ readme-phong.md
@@ -0,0 +1,2 @@
+CFLAGS="-Wno-error=deprecated" make preonic/rev3:phong
+CFLAGS="-Wno-error=deprecated" make preonic/rev3:phong:flash (then press reset on keyboard)
